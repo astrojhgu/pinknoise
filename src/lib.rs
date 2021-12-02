02c69f4ff4e4ff64c4f15e2ae5c5c3726167a6d6
GU Junhua <jhgu@nao.cas.cn>
2021-12-02 21:28:55 +0800

    modified:   Cargo.toml 	modified:   src/lib.rs 	modified:   src/main.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,84 +1,72 @@
-use std::{
-    iter::{
-        Sum
-    }
-};
+use serde::{Deserialize, Serialize};
+use std::iter::Sum;
 
-use num::{
-    traits::{
-        Float
-    }
-};
+use num::traits::Float;
 
-use rand::{
-    Rng, 
-    distributions::{
-        uniform::{
-            SampleUniform
-        }
-    }
-};
+use rand::{distributions::uniform::SampleUniform, Rng};
 
+use rand_distr::{Distribution, StandardNormal};
 
-use rand_distr::{
-    StandardNormal
-    , Distribution
-};
-
-fn which_to_roll(mut n: usize)->usize{
-    let mut x=0;
-    while n&1==0{
-        x+=1;
-        n>>=1;
+fn which_to_roll(mut n: usize) -> usize {
+    let mut x = 0;
+    while n & 1 == 0 {
+        x += 1;
+        n >>= 1;
     }
     x
 }
 
-
-#[derive(Clone, Copy)]
-pub struct VmPinkRng<T,const N:usize>
-where T: Copy
+#[derive(Debug, PartialEq, Serialize, Deserialize, Clone)]
+pub struct VmPinkRng<T>
+where
+    T: Copy,
 {
-    state: [T;N],
+    order: usize,
+    state: Vec<T>,
     cnt: usize,
     norm: T,
 }
 
-impl<T, const N: usize> VmPinkRng<T, N>
-where T:Copy+SampleUniform+Sum+Float
-, StandardNormal: Distribution<T>
+impl<T> VmPinkRng<T>
+where
+    T: Copy + SampleUniform + Sum + Float,
+    StandardNormal: Distribution<T>,
 {
-    pub fn from_state(state: [T; N])->VmPinkRng<T, N>{
-        VmPinkRng{
-            state, 
-            cnt: 0, 
-            norm:T::one()/T::from(N).unwrap().sqrt()
+    pub fn from_state(state: &[T]) -> VmPinkRng<T> {
+        VmPinkRng {
+            order: state.len(),
+            state: Vec::from(state),
+            cnt: 0,
+            norm: T::one() / T::from(state.len()).unwrap().sqrt(),
         }
     }
 
-
-    pub fn new<R>(rng: &mut R)->VmPinkRng<T, N>
-    where R: Rng
+    pub fn new<R>(order: usize, rng: &mut R) -> VmPinkRng<T>
+    where
+        R: Rng,
     {
-        let mut state=[T::zero();N];
-        state.iter_mut().for_each(|x| *x=rng.sample(StandardNormal));
-        Self::from_state([T::zero();N])
+        let mut state = vec![T::zero(); order];
+        state
+            .iter_mut()
+            .for_each(|x| *x = rng.sample(StandardNormal));
+        Self::from_state(&state)
     }
 
-    pub fn from_zero()->VmPinkRng<T, N>{
-        Self::from_state([T::zero();N])
+    pub fn from_zero(order: usize) -> VmPinkRng<T> {
+        Self::from_state(&vec![T::zero(); order])
     }
 
-    pub fn get<R>(&mut self, rng: &mut R)->T
-    where  R: Rng
+    pub fn get<R>(&mut self, rng: &mut R) -> T
+    where
+        R: Rng,
     {
-        for _i in 0..2{
-            self.cnt+=1;
-            let n=which_to_roll(self.cnt);
-            if n<N{
-                self.state[n]=rng.sample(StandardNormal);
-            }    
+        for _i in 0..2 {
+            self.cnt += 1;
+            let n = which_to_roll(self.cnt);
+            if n < self.order {
+                self.state[n] = rng.sample(StandardNormal);
+            }
         }
-        self.state.iter().cloned().sum::<T>()*self.norm
+        self.state.iter().cloned().sum::<T>() * self.norm
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,15 @@
 #![allow(unused_variables)]
 
 extern crate pinknoise;
-use rand::{
-    thread_rng
-};
+use rand::thread_rng;
 
-use pinknoise::{
-    VmPinkRng
-};
+use pinknoise::VmPinkRng;
 
 fn main() {
-    let mut rng=thread_rng();
-    let mut vmpn=VmPinkRng::<f64, 48>::new(&mut rng);
-    for i in 0..65536{
-        let x=vmpn.get(&mut rng);
+    let mut rng = thread_rng();
+    let mut vmpn = VmPinkRng::<f64>::new(48, &mut rng);
+    for i in 0..65536 {
+        let x = vmpn.get(&mut rng);
         println!("{}", x);
     }
 }
